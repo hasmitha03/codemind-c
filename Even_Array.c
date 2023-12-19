1bd10820c8c831d4698fdb07172fafe071a0c924
hasmitha03 <147423248+hasmitha03@users.noreply.github.com>
2023-12-19 09:57:17 +0530

    #include<stdio.h> int main() { 	int a,i; 	scanf("%d",&a); 	int arr[a]; 	for(i=0;i<a;i++) 	{ 		scanf("%d",&arr[i]); 	} 	int allEven =1; 	for(i=0;i<a;i++)
    		if(arr[i] % 2 != 0)
    		{
                allEven = 0;
                break;
    		    
    		}
    		if (allEven==0) 
    		{
            printf("False
    ");
        } 
        else {
            printf("True
    ");
        }
    	
    }

---
+++ Even_Array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int a,i;
+	scanf("%d",&a);
+	int arr[a];
+	for(i=0;i<a;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int allEven =1;
+	for(i=0;i<a;i++)
+	
+		if(arr[i] % 2 != 0)
+		{
+            allEven = 0;
+            break;
+		    
+		}
+		if (allEven==0) 
+		{
+        printf("False
+");
+    } 
+    else {
+        printf("True
+");
+    }
+	
+}
